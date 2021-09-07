76b0f8a3554ad1753c9bbb2e024453f258d34e6b
Lin Xiaotian <sky.linxiaotian@gmail.com>
2021-09-07 10:58:37 +0800

    stack_list

--- data_structure/stack/stack_list.c
+++ data_structure/stack/stack_list.c
@@ -7,6 +7,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 typedef struct ListNode {
     int val;
@@ -32,7 +33,77 @@ void freeNode(Node *n) {
 
 Stack *initStack() {
     Stack *s = (Stack *)malloc(sizeof(Stack));
-    s->head = NULL;
+    s->head = (Node *)malloc(sizeof(Node));
+    s->head->next = NULL;
     s->len = 0;
     return s;
+}
+
+void freeStack(Stack *s) {
+    if (!s) return;
+    Node *p = s->head;
+    Node *remove;
+    while(p) {
+        remove = p;
+        p = p->next;
+        free(remove);
+    }
+    return;
+}
+
+int push(Stack *s, int val) {
+    if (!s) return 0;
+    Node *n = initNode(val);
+    n->next = s->head->next;
+    s->head->next = n;
+    s->len++;
+    return 1;
+}
+
+int isEmpty(Stack *s) {
+    return !s->len;
+}
+
+int pop(Stack *s) {
+    int val = s->head->next->val;
+    Node *p = s->head->next;
+    s->head->next = p->next;
+    free(p);
+    s->len--;
+    return val;
+}
+
+void showStack(Stack *s) {
+    if (!s) return;
+    Node *p = s->head->next;
+    printf("Stack:[");
+    while(p) {
+        printf("%d->", p->val);
+        p = p->next;
+    }
+    printf("]\n");
+    printf("-------------\n");
+    return;
+}
+
+int main() {
+    srand(time(0));
+    Stack *s = initStack();
+    int cnt = 20;
+    while (cnt--) {
+        int val = rand() % 100;
+        int opt = rand() % 4;
+        switch (opt) {
+            case 0:
+            case 1:
+            case 2:
+                printf("push %d, res = %s\n", val, push(s, val) ? "SUC" : "ERR");
+                break;
+            case 3:
+                isEmpty(s) ? printf("pop nothing\n") : printf("pop %d\n", pop(s));
+        }
+        showStack(s);
+    }
+    freeStack(s);
+    return 0;
 }
